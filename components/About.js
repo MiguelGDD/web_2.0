afb0c406c28bc0c803c0dd8b424f4ff9b62aa625
Miguel Guell <miguel.guell@unisimon.edu.co>
2022-06-12 18:08:02 -0500

    implement design of about

--- components/About.js
+++ components/About.js
@@ -1,14 +1,55 @@
+import Image from 'next/image'
 import React from 'react'
+import fotoMiguel from '../public/miguel-foto.jpg'
 
 const About = () => {
   return (
-    <>
-    <div className='flex flex-row'>
-    <div></div>
-    <div></div>
-    <div></div>
+    <div className='w-8/12 m-auto flex flex-row mt-36 justify-between items-center '>
+      <div className='w-1/3 text-center flex flex-col h-full my-auto'>
+        <div>
+          <div>title</div>
+          <div>Lorem ipsum dolor sit amet</div>
+        </div>
+
+        <div>
+          <div>title</div>
+          <div>Lorem ipsum dolor sit amet</div>
+        </div>
+
+        <div>
+          <div>title</div>
+          <div>Lorem ipsum dolor sit amet</div>
+        </div>
+      </div>
+
+      <div className='w-1/5'>
+        <div className='flex flex-col h-auto overflow-hidden w-full rounded-full '>
+          <Image src={fotoMiguel} />
+        </div>
+        <div className='items-center justify-center text-center mt-5'>
+          <p>Miguel Guell</p>
+          <p>Frontend Developer</p>
+        </div>
+      </div>
+
+      <div className='w-1/3 text-center flex flex-col h-full my-auto '>
+        <div>
+          <div>title</div>
+          <div>Lorem ipsum dolor sit amet</div>
+        </div>
+
+        <div>
+          <div>title</div>
+          <div>Lorem ipsum dolor sit amet</div>
+        </div>
+
+        <div>
+          <div>title</div>
+          <div>Lorem ipsum dolor sit amet</div>
+        </div>
+      </div>
+      
     </div>
-    </>
   )
 }
 
--- components/Header.js
+++ components/Header.js
@@ -4,21 +4,27 @@ import Logo from '../public/logo.png';
 
 function Header() {
   return (
-    <nav className="h-28 w-full  overflow-hidden flex justify-center">
-      <div className="mt-2 max-w-7xl w-full flex ">
-        <div className='flex ml-auto'>
-          <Image src={Logo} width={'120px'} />
-        </div>
-        <div>
-          <ul className="flex ml-32 mt-8">
-            <li className="mx-10 cursor-pointer"><a>About</a></li>
-            <li className="mx-10 cursor-pointer"><a>Projects</a></li>
-            <li className="mx-10 cursor-pointer"><a>Contacto</a></li>
-          </ul>
-        </div>
+    <>
+      <div className='fixed left-40'>
+            <Image src={Logo} width={'120px'} height={'120px'}/>
       </div>
+      <nav className="h-28 w-full overflow-hidden flex justify-center">
+        <div className="mt-2 max-w-7xl w-full flex justify-between">
+          <div>
+            <p>{'q'}</p>
+          </div>
+          <div>  
+            <ul className="flex ml-32 mt-8 justify-end font-bold">
+              <li className="mx-10 cursor-pointer"><a>About</a></li>
+              <li className="mx-10 cursor-pointer"><a>Projects</a></li>
+              <li className="mx-10 cursor-pointer"><a>Contacto</a></li>
+            </ul>
+          </div>
+        </div>
 
-    </nav>
+      </nav>
+      
+  </>
   );
 }
 
